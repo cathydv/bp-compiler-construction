a31cf79b252e7affb20db1309e094f2fe74ade55
compiler <compiler@compiler-laptop>
2012-04-17 20:50:29 +0200

    fixed minor issues and pretty print of symboltable - finally works perfectly!!

--- dhbwCompiler/src/symboltable.c
+++ dhbwCompiler/src/symboltable.c
@@ -21,23 +21,24 @@ void pushVar(char const *name) {
 	s->name = (char *) malloc (strlen (name) + 1);
 	s->name = name;
 	s->type = 1;
-	s->var.scope=currentScope;
-	printf("identified a variable %s in the scope %s\n",s->name,s->var.scope->name);
+	if(currentScope!=NULL){
+		s->var.scope=currentScope;
+		printf("identified a variable \"%s\" in the scope of function \"%s\"\n",s->name,s->var.scope->name);
+	}
+	else printf("identified a global variable \"%s\"\n",s->name);
+
 	if(currentScope==NULL)
 		if(!exists_Sym_glob(name)){
-			printf("appending sym %s to global table \n",name);
+			printf("appending symbol \"%s\" to global symboltable \n",name);
+			printf("-----------------------------------------------------------------\n");
 			LL_APPEND(symtable,s);
 		}
 		else return;
 	else if(!exists_Sym_loc(name)){
-		printf("appending sym %s to local table %s \n",name,currentScope->name);
+		printf("appending symbol \"%s\" to local table \"%s\" \n",name,currentScope->name);
+		printf("-----------------------------------------------------------------\n");
 		LL_APPEND(currentScope,s);
 	}
-}
-
-void resetScope(){
-	currentScope=NULL;
-	printf("reset scope to global\n");
 }
 
 void pushFunc(int type, char const *name){
@@ -51,17 +52,24 @@ void pushFunc(int type, char const *name){
 	if(!exists_Sym_glob(name)){
 		LL_APPEND(symtable,s);
 		printf("appending function %s to global table \n",name);
+		printf("-----------------------------------------------------------------\n");
 	}
 
 	currentScope = s;
 	printf("set current scope to %s\n",currentScope->name);
+	printf("-----------------------------------------------------------------\n");
 
 }
 
+void resetScope(){
+	currentScope=NULL;
+	printf("reset scope to global\n");
+	printf("-----------------------------------------------------------------\n");
+}
 
 int exists_Sym_glob(char const *name){
 	symbol *s = NULL;
-	printf("searching for symbol %s in global table\n",name);
+	printf("searching for symbol \"%s\" in global table\n",name);
 	if(symtable == NULL){
 		printf("global symboltable is empty\n");
 		return 0;
@@ -69,7 +77,7 @@ int exists_Sym_glob(char const *name){
 
 	LL_FOREACH(symtable,s){
 			if (! strcmp(name, s->name)){
-				printf("ERROR	--	multiple declaration of variable \"%s\n", name);
+				printf("ERROR	--	multiple declaration of variable \"%s\"\n", name);
 					return 1;
 				}
 	}
@@ -80,9 +88,9 @@ int exists_Sym_glob(char const *name){
 
 int exists_Sym_loc(char const *name){
 	symbol *s = NULL;
-	printf("searching for symbol %s in table\n",name);
+	printf("searching for symbol \"%s\" in table \"%s\"\n",name,currentScope->name);
 	if(currentScope->next == NULL){
-		printf("local table %s is empty\n",currentScope->name);
+		printf("local table \"%s\" is empty\n",currentScope->name);
 		return 0;
 	}
 
@@ -97,8 +105,6 @@ int exists_Sym_loc(char const *name){
 }
 
 
-
-
 struct Symbol* find_Sym(char const *name){
 	symbol *s = NULL;
 	LL_FOREACH(symtable,s)
@@ -111,11 +117,16 @@ struct Symbol* find_Sym(char const *name){
 void debug_printSymbolTable(){
 	symbol *s = NULL;
 
-	printf("\n\n - DEBUG __ SYMBOL ___ TABLE - \n\n ");
+	printf("\n\n\t\t - DEBUG ___ SYMBOL _ TABLE ___ DEBUG - \n\n");
+	printf("-----------------------------------------------------------------\n");
+	printf("     global \t\t|     local\n");
+	printf("-----------------------------------------------------------------\n");
 
 	LL_FOREACH(symtable,s)
 		if(s->var.scope==NULL)
 			printf("|type:%d name:%s| \n",s->type,s->name);
+		else
+			printf("\t\t\t|type:%d name:%s| \n",s->type,s->name);
 
 //		else
 //			printf("|type:int value:%d name:%s scope:%s| \n ",s->var.value,s->name,s->scope->name);
