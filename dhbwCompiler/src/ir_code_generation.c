3f38f1864bd5bbaf578360ec17690b1ed9ddd538
seven89 <compiler@compiler-laptop>
2012-04-20 00:10:04 +0200

    ir_code_generation.c - temporäre variablen

--- dhbwCompiler/src/ir_code_generation.c
+++ dhbwCompiler/src/ir_code_generation.c
@@ -5,4 +5,17 @@
  *      Author: FS
  */
 
+#include "ir_code_generation.h"
+#include "symboltable.h"
+#include "parser.tab.h"
+#include "stdio.h"
+#include "stdlib.h"
 
+//temporäre Variablen (5)
+struct symbolTemp *irtemp()
+{
+	char buffer [5];
+	struct symbol temp;
+	temp = tempSym(buffer);
+	return (temp);
+}
